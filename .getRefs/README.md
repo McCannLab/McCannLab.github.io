b6633f6682b0d321f0c24ba60bc2ee64d3a45bad
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2020-09-10 10:04:04 -0400

    added help comments on getRefs files

---
+++ .getRefs/README.md
@@ -0,0 +1,12 @@
+# getRefs README
+
+The two files in this folder pull Kevin's papers and processes the references into a form that the hugo program can use to populate the Publications page.
+
+REQUIRES
+
+R
+devtools (R library) plus system dependencies
+refR (R library - https://github.com/inSileco/refR)
+pandoc
+pandoc-citeproc
+git
--- .getRefs/getRefs.R
+++ .getRefs/getRefs.R
@@ -1,9 +1,14 @@
-#
+# This R script pulls all of Kevin's papers from SCOPUS then combines them into a refs.bib file.
+
+# Uses the R library refR from https://github.com/inSileco/refR
+# You will need to install the R library devtools (and any system dependencies) to get refR from GitHub
+
+#You will also need to grab the API key associated with the email account mccannlb@uoguelph.ca (in the mccannlab computer accounts keepassxc - or in the mccannlb account on https://dev.elsevier.com/)
 
 setwd("/McCannLab/getRefs/yamlfiles")
 library("refR")
 
-api_key   <- ""
+api_key   <- "1234" #replace 1234 with the api_key
 refs <- refR::getRefs(
   api_key = api_key,
   author_id = "7005189478",
